8b906b4c183e477f2dd9317f73d79ce71f5666fe
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-01-21 21:49:59 -0300

    Adiciona variável ambiente pra URL da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://omnistack-10-dev-radar-backend.herokuapp.com/'
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
